62d890c86b6f001d36069b36c801708eb4f97af6
Olga Bakhur <olgabakhur123@gmail.com>
2020-04-03 18:07:56 +0300

    hw

--- Домашнее задание 4 (30.03.2020)/Calculator/src/com/company/CalcWithCounter.java
+++ Домашнее задание 4 (30.03.2020)/Calculator/src/com/company/CalcWithCounter.java
@@ -1,4 +1,45 @@
 package com.company;
 
-public class CalcWithCounter {
+public class CalcWithCounter implements ICalc {
+
+    private ICalc calcAll;
+    public CalcWithCounter (ICalc a){
+        this.calcAll  = a;
+    }
+
+    private double counter = 0;
+
+    public double adding (double x, double y) {
+        this.counter++;
+        return x + y;
+    }
+
+    public double subtraction (double x, double y) {
+        this.counter++;
+        return x - y;
+    }
+
+    public double division (double x, double y){
+        this.counter++;
+        return x / y;
+    }
+
+    public double multiplication (double x, double y){
+        this.counter++;
+        return x * y;
+    }
+
+    public double power(double base, double power) {
+        this.counter++;
+        return Math.pow(base, power);
+    }
+
+    public double module (double a) {
+        this.counter++;
+        return Math.abs(a);
+    }
+
+    public double getCounter(){
+        return this.counter;
+    }
 }
--- Домашнее задание 4 (30.03.2020)/Calculator/src/com/company/CalcWithMath.java
+++ Домашнее задание 4 (30.03.2020)/Calculator/src/com/company/CalcWithMath.java
@@ -1,28 +1,40 @@
 package com.company;
 
 public class CalcWithMath implements ICalc{
+
+    private double counter = 0;
+
     public double adding(double x, double y) {
+        this.counter++;
         return x + y;
     }
 
     public double subtraction (double x, double y) {
+        this.counter++;
         return x - y;
     }
 
     public double division (double x, double y){
+        this.counter++;
         return x / y;
     }
 
-
     public double multiplication (double x, double y){
+        this.counter++;
         return x * y;
     }
 
     public double power (double base, double power) {
+        this.counter++;
         return Math.pow(base, power);
     }
 
     public double module (double a) {
+        this.counter++;
         return Math.abs(a);
+    }
+
+    public double getCounter(){
+        return this.counter;
     }
 }
